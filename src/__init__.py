2519cf7d1be67f5dd800b15ac6728f909cff865c
MrPio <valeriomorelli50@gmail.com>
2025-07-31 15:27:13 +0200

    Add DeepCAD comparison

--- src/__init__.py
+++ src/__init__.py
@@ -4,3 +4,15 @@ __license__ = "Apache-2.0"
 
 
 from .utils import imshow, cprint, log
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({
+    "font.size": 13,
+    "axes.titlesize": 13,
+    "axes.labelsize": 13,
+    "axes.grid": True,
+    # "axes.facecolor": "#222222",  # dark background
+    # "grid.color": "#444444",
+    "grid.linestyle": "--",
+    # "figure.facecolor": "#222222",
+})
