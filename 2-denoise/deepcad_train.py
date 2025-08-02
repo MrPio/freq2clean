1c9a34c239356d20a1d9978335c522c7bac1e629
MrPio <valeriomorelli50@gmail.com>
2025-08-02 15:08:46 +0200

    Remove necessity for tiff samples

--- 2-denoise/deepcad_train.py
+++ 2-denoise/deepcad_train.py
@@ -28,7 +28,7 @@ training_class(
         "b1": 0.5,  # Adam: bata1
         "b2": 0.999,  # Adam: bata2
         "fmap": 16,  # model complexity
-        "GPU": "0,1",  # GPU index
+        "GPU": "0",  # GPU index
         "num_workers": 0,  # if you use Windows system, set this to 0.
         "visualize_images_per_epoch": False,  # whether to show result images after each epoch
         "save_test_images_per_epoch": True,  # whether to save result images after each epoch
--- src/recording.py
+++ src/recording.py
@@ -7,9 +7,13 @@ from os import PathLike
 
 
 class Recording:
-    def __init__(self, video: PathLike | np.ndarray):
-        self.video = video if isinstance(video, np.ndarray) else tiff.imread(str(video))
-        self.video = self.video.astype(np.float64) / np.max(self.video)
+    def __init__(self, video: PathLike | np.ndarray, max_length: int = 1000):
+        self.video = (
+            video
+            if isinstance(video, np.ndarray)
+            else tiff.imread(str(video), key=range(max_length) if max_length else None)
+        )
+        self.video = (self.video / np.max(self.video)).astype(np.float32)
 
     @property
     def frames(self) -> int:
@@ -20,7 +24,7 @@ class Recording:
         return np.clip(normalize(self.video, 0.5, 99.5), max=1)
 
     def save_sample(self, path: Path | str, length=500):
-        tiff.imwrite(str(path), self.video[:length], dtype=np.float64)
+        tiff.imwrite(str(path), self.video[:length], dtype=np.float32)
 
     def render(self, path: Path | str, start=None, end=None, bitrate=4500, fps=30):
         iio.imwrite(
@@ -31,3 +35,6 @@ class Recording:
             bitrate=f"{bitrate}k",
             output_params=["-loglevel", "quiet"],
         )
+
+    def __getitem__(self, i):
+        return self.video[i]
