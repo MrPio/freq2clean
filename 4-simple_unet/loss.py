New trainings (4) with bi-loss and Fix Dataset normalization

4-simple_unet/loss.py
@@ -13,25 +13,25 @@ def __make_gaussian_kernel(channels, kernel_size=5, sigma=1.0, device="cuda"):
     return kernel2d  # (channels, 1, kernel_size, kernel_size)
 
 
-def lf_hf_tv_loss(I, I_c, I_n, lambda_lf=1.0, lambda_hf=0.5, lambda_tv=1e-4, kernel_size=5, sigma=1.0):
-    B, C, H, W = I.shape
-    device = I.device
+def lf_hf_tv_loss(pred, clean, noisy, lambda_lf=1.0, lambda_hf=0.5, lambda_tv=1e-4, kernel_size=5, sigma=1.0):
+    B, C, H, W = pred.shape
+    device = pred.device
 
     # low-pass filter
     gauss_k = __make_gaussian_kernel(C, kernel_size, sigma, device=device)
-    lpf_I = F.conv2d(I, gauss_k, padding=kernel_size // 2, groups=C)
-    lpf_Ic = F.conv2d(I_c, gauss_k, padding=kernel_size // 2, groups=C)
+    lpf_I = F.conv2d(pred, gauss_k, padding=kernel_size // 2, groups=C)
+    lpf_Ic = F.conv2d(clean, gauss_k, padding=kernel_size // 2, groups=C)
     # high-pass = residual
-    hpf_I = I - lpf_I
-    hpf_In = I_n - F.conv2d(I_n, gauss_k, padding=kernel_size // 2, groups=C)
+    hpf_I = pred - lpf_I
+    hpf_In = noisy - F.conv2d(noisy, gauss_k, padding=kernel_size // 2, groups=C)
 
     # LF & HF MSE
     loss_lf = F.mse_loss(lpf_I, lpf_Ic)
     loss_hf = F.mse_loss(hpf_I, hpf_In)
 
     # anisotropic total variation
-    tv_h = torch.abs(I[:, :, :, :-1] - I[:, :, :, 1:]).mean()
-    tv_v = torch.abs(I[:, :, :-1, :] - I[:, :, 1:, :]).mean()
+    tv_h = torch.abs(pred[:, :, :, :-1] - pred[:, :, :, 1:]).mean()
+    tv_v = torch.abs(pred[:, :, :-1, :] - pred[:, :, 1:, :]).mean()
     loss_tv = tv_h + tv_v
 
     return lambda_lf * loss_lf + lambda_hf * loss_hf + lambda_tv * loss_tv

4-simple_unet/test.py
@@ -1,5 +1,5 @@
 """
-Usage: python test.py --checkpoint="pth/202508041859/8.pt"
+Usage: python test.py --checkpoint="pth/202508051638/4.pt"
 """
 
 import argparse
@@ -45,19 +45,17 @@ model.load_state_dict(state_dict)
 model.to(device).eval()
 
 cprint("green:Loading dataset...")
+ds = AstroDataset()
 rec = Recording(DATASETS["oabf_astro"] / "y.tiff", max_frames=300)
 frames = rec.frames
 rec = rec.np
-# rec = (rec - np.min(rec)) / (np.max(rec) - np.min(rec))
-# rec = rec * 2 - 1
-rec = np.clip(normalize(rec, 0.1, 99.9), max=1)
 
 cprint("yellow:Starting training...")
 start_time = time_ns()
-preds = np.empty_like(rec)
+preds = np.empty_like(rec, dtype=np.float32)
 for i in tqdm(range(0, frames, BS), desc="Frames"):
     x = rec[i : i + BS]
-    x = transforms.Normalize(0.5, 0.5)(torch.tensor(x)).unsqueeze(1).to(model.device)
+    x = ds.clean_transforms(x).unsqueeze(1).to(model.device)
     with torch.no_grad():
         pred = model(x, torch.zeros(x.shape[0]).to(model.device)).sample
         preds[i : i + BS] = pred.cpu().squeeze(1).numpy()

4-simple_unet/train.py
@@ -18,8 +18,8 @@ sys.path.append(str(Path(__file__).resolve().parents[1]))
 from src import *
 
 EPOCHS = 10
+SAVE_EVERY = 250
 BS = 16
-TRAINSET = "dataset/astro_1"
 
 
 def gradient_loss(pred, gt):
@@ -36,7 +36,7 @@ model = DeepCADImprovementUNet()
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
 
 cprint("green:Loading dataset...")
-dataset = Dataset2PM(TRAINSET, augument=False)
+dataset = AstroDataset(augument=False)
 dataloader = DataLoader(dataset, batch_size=BS, shuffle=True, num_workers=1)
 
 cprint("blue:Loading accelerator...")
@@ -50,6 +50,7 @@ pth_dir = Path(f"pth/{train_name}")
 (pth_dir / "snaps").mkdir(parents=True, exist_ok=True)
 
 cprint("yellow:Starting training...")
+checkpoints = 0
 for epoch in tqdm(range(EPOCHS), desc="Epochs"):
     with tqdm(dataloader, leave=False, disable=not accelerator.is_main_process) as pbar:
         for gt, x in pbar:
@@ -60,7 +61,10 @@ for epoch in tqdm(range(EPOCHS), desc="Epochs"):
             ssim = pytorch_msssim.ssim(pred, gt, data_range=2.0, size_average=True)
             grad = gradient_loss(pred, gt)
             # loss = 1 * mse + 0.1 * l1 + 0.55 * (1 - ssim) + 0.1 * grad
-            loss = lf_hf_tv_loss(pred, x, gt)
+            loss = lf_hf_tv_loss(pred, x, gt, lambda_hf=1.85, lambda_tv=1.25e-4)
+
+            # ssim_x = pytorch_msssim.ssim(pred, x, data_range=2.0, size_average=True)
+            # loss = mse_loss(pred, x) + (1 - ssim_x) + 0.5 * l1_loss(pred, gt)
 
             accelerator.backward(loss)
             optimizer.step()
@@ -72,19 +76,22 @@ for epoch in tqdm(range(EPOCHS), desc="Epochs"):
             h, m, s = delta // 3600, (delta % 3600) // 60, delta % 60
             pbar.set_postfix(
                 {
-                    f"Loss": f"{loss.item():.2f} (L1:{l1.item():.2f}, SSIM:{ssim.item():.2f}, Grad:{grad.item():.2f}, MSE:{mse.item():.2f})",
+                    f"Loss": f"{loss.item():.3f} (L1:{l1.item():.2f}, SSIM:{ssim.item():.2f}, Grad:{grad.item():.2f}, MSE:{mse.item():.2f})",
                     "Free VRAM": f"{vram_free:.1f}GiB",
                     "Time": f"{h:.0f}h {m:.0f}m {s:.0f}s",
                 }
             )
             metrics.loc[len(metrics)] = [loss.item(), l1.item(), ssim.item(), grad.item(), mse.item()]
 
-            # Preview images
+            # Save preview image and metrics
             if (len(metrics) - 1) % 50 == 0 and accelerator.is_main_process:
-                pil_stack(map(tensor2pil, [x[0], gt[0], pred[0]])).save(f"pth/{train_name}/snaps/{len(metrics)}.png")
+                metrics.to_parquet(pth_dir / f"metrics.parquet")
+                pil_stack(map(tensor2pil, [gt[0], x[0], pred[0]])).save(f"pth/{train_name}/snaps/{len(metrics)}.png")
 
-        accelerator.wait_for_everyone()
-        if accelerator.is_main_process:
-            metrics.to_parquet(pth_dir / f"metrics.parquet")
-            cprint(f"cyan:Saving checkpoint [{epoch+1}/{EPOCHS}]")
-            torch.save(model.state_dict(), pth_dir / f"{epoch}.pt")
+            # Save checkpoint
+            if (len(metrics)) % SAVE_EVERY == 0:
+                accelerator.wait_for_everyone()
+                if accelerator.is_main_process:
+                    checkpoints += 1
+                    cprint(f"cyan:\nSaving checkpoint [{checkpoints+1}]")
+                    torch.save(model.state_dict(), pth_dir / f"{checkpoints}.pt")

src/dataset.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
@@ -5,6 +6,7 @@ from PIL import Image
 import os
 import random
 import sys
+from pathlib import Path
 
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 from src import *
@@ -22,8 +24,10 @@ def random_transform(input, target):
     Return:
         input, target : the input and target patch after data augmentation
     """
-    p_trans = random.randrange(1, 8)
-    if p_trans == 1:  # left rotate 90
+    p_trans = random.randrange(0, 8)
+    if p_trans == 0:
+        pass
+    elif p_trans == 1:  # left rotate 90
         input = torch.rot90(input, k=1, dims=(1, 2))
         target = torch.rot90(target, k=1, dims=(1, 2))
     elif p_trans == 2:  # left rotate 180
@@ -54,33 +58,50 @@ def random_transform(input, target):
 
 
 class Dataset2PM(Dataset):
-    def __init__(self, dir, augument=True):
-        self.root = dir
-        self.transforms = transforms.Compose(
-            [
-                transforms.Resize(512),
-                transforms.CenterCrop(512),
-                transforms.ToTensor(),  # [0,1]
-                transforms.Normalize(0.5, 0.5),  # [–1,1]
-            ]
-        )
-        # assume structure root/noisy/*.png and root/cond/*.png
-        self.noisy_paths = sorted(os.listdir(os.path.join(dir, "noisy")))
-        self.cond_paths = sorted(os.listdir(os.path.join(dir, "cond")))
+    def __init__(self, dir: Path | str, noisy_transforms, clean_transforms, augument=True):
+        self.dir = Path(dir)
+        self.noisy_transforms = noisy_transforms
+        self.clean_transforms = clean_transforms
+        self.noisy_paths = sorted((self.dir / "noisy").glob("*.png"))
+        self.clean_paths = sorted((self.dir / "clean").glob("*.png"))
         self.augument = augument
 
     def __len__(self):
         return len(self.noisy_paths)
 
     def __getitem__(self, idx):
-        noisy = Image.open(os.path.join(self.root, "noisy", self.noisy_paths[idx])).convert("L")
-        cond = Image.open(os.path.join(self.root, "cond", self.cond_paths[idx])).convert("L")
-        noisy = self.transforms(noisy)
-        cond = self.transforms(cond)
+        noisy = np.array(Image.open(self.noisy_paths[idx]))
+        clean = np.array(Image.open(self.clean_paths[idx]))
+        noisy = self.noisy_transforms(noisy)
+        clean = self.clean_transforms(clean)
         if self.augument:
-            if random.random() < 0.67:
-                noisy, cond = random_transform(noisy, cond)
-        return noisy, cond
+            noisy, clean = random_transform(noisy, clean)
+        return noisy, clean
+
+
+class AstroDataset(Dataset2PM):
+    def __init__(self, augument=True):
+        noisy_transforms = transforms.Compose(
+            [
+                # numpy uint16 H×W -> torch.Tensor 1×H×W [0,1]
+                transforms.Lambda(lambda x: torch.from_numpy(x).float() / float(14_207)),
+                transforms.Lambda(lambda x: torch.clip(x, min=0, max=1)),
+                transforms.Lambda(lambda t: t.unsqueeze(0) if t.ndim == 2 else t),
+                # normalize  [0,1] -> [–1,1]
+                transforms.Normalize(mean=0.5, std=0.5),
+            ]
+        )
+        clean_transforms = transforms.Compose(
+            [
+                # numpy uint16 H×W -> torch.Tensor 1×H×W [0,1]
+                transforms.Lambda(lambda x: torch.from_numpy(x).float() / float(6_521)),
+                transforms.Lambda(lambda x: torch.clip(x, min=0, max=1)),
+                transforms.Lambda(lambda t: t.unsqueeze(0) if t.ndim == 2 else t),
+                # normalize  [0,1] -> [–1,1]
+                transforms.Normalize(mean=0.5, std=0.5),
+            ]
+        )
+        super().__init__("dataset/oabf_astro", noisy_transforms, clean_transforms, augument)
 
 
 class DatasetNextFrame(Dataset):