cbe2a828426d9bc53c966a2cfb88418371ab6d59
MrPio <valeriomorelli50@gmail.com>
2025-11-10 16:15:38 +0100

    edit freq2clean training script

--- 5-freq2clean/freq2clean.py
+++ 5-freq2clean/freq2clean.py
@@ -5,17 +5,29 @@ import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader
 import sys
 from csbdeep.utils import normalize
+from scipy.ndimage import uniform_filter1d
 
 sys.path.append("..")
 from src import *
 
 
+def match(src, trg):
+    low = torch.quantile(src.flatten(), 0.25)
+    high = torch.quantile(src.flatten(), 0.75)
+    t_low = torch.quantile(trg.flatten(), 0.25)
+    t_high = torch.quantile(trg.flatten(), 0.75)
+    denom = (high - low).clamp(min=1e-6)
+    a = (t_high - t_low) / denom
+    b = t_low - a * low
+    return torch.clamp(a * src + b, 0, 1)
+
+
 class Freq2Clean(nn.Module):
     def __init__(self, frames, alphas=None):
         super().__init__()
-
+        self.frames = frames
         if not alphas:
-            alphas = [1] + [0.01] * (frames - 1)
+            alphas = [0.85] + [0.0] * (frames - 1)
         self.alphas = nn.Parameter(torch.tensor(alphas))
 
     def forward(self, y_hat: torch.Tensor, y_bar: torch.Tensor) -> torch.Tensor:
@@ -27,88 +39,105 @@ class Freq2Clean(nn.Module):
         Y_hat_angle = torch.angle(Y_hat)
         # alpha_factor = torch.clamp(self.alphas, 0.0, 1.0) # Clamping for stability
 
+        freq0 = match(torch.mean(y_bar, axis=1), Y_hat_abs[:, 0] / self.frames) * self.frames
+        Y_bar_abs[:, 0] = freq0
+
         Y_abs = Y_bar_abs * self.alphas.view(1, -1, 1, 1) + Y_hat_abs * (1 - self.alphas).view(1, -1, 1, 1)
         Y = torch.polar(Y_abs, Y_hat_angle)
         return torch.fft.ifft(Y, dim=1).real
 
-# TODO: OVERLAP Avg l1
-# Args 
 
+# Args
 denoiser_name: Literal["deepcad", "noise2noise", "noise2void"] = "deepcad"
 denoiser_variant = "_15"
 dataset = "synthetic"
 CUPY_AVAILABLE = False
 PATCH_T = 600
-BATCH_SIZE = 1
+AVG_WIN = 2048
+BATCH_SIZE = 2
 EPOCHS = 100
-LEARNING_RATE = 0.01
+LEARNING_RATE = 0.0005
 SAVE_SNAPS = True
+METRICS_PATH = Path(f"f2c_{dataset}_metrics_{denoiser_name}.csv")
+RES_DIR = mkdir(f"results/{dataset}/")
+MAX_FRAMES = 3000
 device = "cuda"
 snapdir = mkdir("snapshots")
+alphadir = mkdir("snapshots/alphas")
 
 
-def normalize(t):
-    t_min = t.min()
-    t_max = t.max()
-    print(t_min, t_max)
-    return 2 * (t - t_min) / (t_max - t_min) - 1
+x = Recording(f"dataset/{dataset}/x.tif", max_frames=MAX_FRAMES).normalized
+y = Recording(f"dataset/{dataset}/{denoiser_name}{denoiser_variant}.tif", max_frames=MAX_FRAMES).normalized
+gt = Recording(f"dataset/{dataset}/gt.tif", max_frames=MAX_FRAMES).normalized
 
+x_bar = uniform_filter1d(x, size=AVG_WIN, axis=0, mode="reflect")
 
-def psnr(y_pred: torch.Tensor, y_true: torch.Tensor, data_range: float = 1.0) -> torch.Tensor:
-    mse = torch.mean((y_pred - y_true) ** 2)
-    if mse < 1e-10:
-        return torch.tensor(999.0, dtype=y_pred.dtype, device=y_pred.device)
-    return 10.0 * torch.log10((data_range**2) / mse)
+overlap = 0.5
+new_patcht = PATCH_T * (1 - overlap)
+discard = math.ceil(1 / (1 - overlap) - 1)
+idx = (np.arange(x.shape[0] // new_patcht)[:-discard, None] * new_patcht + np.arange(PATCH_T)).astype(int)
+x = torch.from_numpy(x[idx]).float()
+x_bar = torch.from_numpy(x_bar[idx]).float()
+y = torch.from_numpy(y[idx]).float()
+gt = torch.from_numpy(gt[idx]).float()
+dataset = TensorDataset(x[1:], x_bar[1:], y[1:], gt[1:])
+validset = (x[0], x_bar[0], y[0], gt[0])
+dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+del x, x_bar, y, gt
+cprint(f"Dataset has", len(idx), "samples.")
 
+model = Freq2Clean(frames=PATCH_T).to(device)
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=1e-6)
+criterion = SSIM3D()  # nn.L1Loss()
+mse = nn.MSELoss()
+lambda_mse = 0.2
+lambda_reg = 1e-4
+init_alphas = torch.tensor([0.85] + [0.0] * (PATCH_T - 1)).to(device)
 
-if __name__ == "__main__":
-    METRICS_PATH = Path(f"f2c_{dataset}_metrics_{denoiser_name}.csv")
-    RES_DIR = mkdir(f"results/{dataset}/")
+for dir in [snapdir, alphadir]:
+    for file in dir.glob("*.png"):
+        file.unlink()
 
-    clog("blue:Loading Freq2Clean...")
-    model = Freq2Clean(frames=PATCH_T).to(device)
-    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
-    criterion=nn.MSELoss()
+pbar = trange(EPOCHS)
+for epoch in pbar:
+    # Validate
+    x, x_bar, y, gt = validset
+    model.eval()
+    f2c = model(y.to(device).unsqueeze(0), x_bar.to(device).unsqueeze(0))
+    loss = -criterion(f2c.unsqueeze(0), gt.to(device).unsqueeze(0).unsqueeze(0))
+    imshow(
+        {
+            "Noisy": x[-1].numpy(),
+            "DeepCAD-RT": y[-1].cpu().numpy(),
+            "AVG": x_bar[-1].cpu().numpy(),
+            f"Freq2Clean ({loss.item():.5f})": f2c[0][-1].cpu().detach().numpy(),
+            "Ground Truth": gt[-1].cpu().numpy(),
+        },
+        path=snapdir / f"{epoch}.png",
+        size=5,
+        vrange=(0, 1),
+    )
+    fig = plt.figure(figsize=(20, 6))
+    pd.Series(model.alphas.cpu().detach().numpy()).plot()
+    fig.savefig(alphadir / f"{epoch}.png", dpi=150, bbox_inches="tight")
+    plt.close(fig)
 
-    clog("red:Loading Normalized dataset...")
-    x = Recording(f"dataset/{dataset}/x.tif", max_frames=None).normalized
-    y = Recording(f"dataset/{dataset}/{denoiser_name}{denoiser_variant}.tif", max_frames=None).normalized
-    gt = Recording(f"dataset/{dataset}/gt.tif", max_frames=None).normalized
-
-    clog("green:Splitting data in batches")
-    idx = np.arange(x.shape[0] // PATCH_T)[:, None] * PATCH_T + np.arange(PATCH_T)
-    x = torch.from_numpy(x[idx]).float()
-    y = torch.from_numpy(y[idx]).float()
-    gt = torch.from_numpy(gt[idx]).float()
-
-    clog("green:Loading DataLoader...")
-    dataset = TensorDataset(x, y, gt)
-    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
-    del x, y, gt
-
-    clog("yellow:Running training...")
-    for epoch in trange(EPOCHS):
+    # Train
+    for i, (x, x_bar, y, gt) in enumerate(dataloader):
         model.train()
-        for i, (x, y, gt) in enumerate(dataloader):
-            optimizer.zero_grad()
-            f2c = model(y.to(device), x.to(device))
-            # loss = -psnr(f2c, gt.to(device), data_range=1.0)
-            loss=criterion(f2c, gt.to(device))
-            
-            loss.backward()
-            optimizer.step()
+        optimizer.zero_grad()
+        f2c = model(y.to(device), x_bar.to(device))
 
-            cprint(f"[{i}/{len(dataloader)}] Loss=", f"cyan:{loss.item():.3f}")
-            if SAVE_SNAPS:
-                imshow(
-                    {
-                        "Noisy": x[0][-1].cpu().numpy(),
-                        "DeepCAD-RT": y[0][-1].cpu().numpy(),
-                        "Freq2Clean": f2c[0][-1].cpu().detach().numpy(),
-                        "Ground Truth": gt[0][-1].cpu().numpy(),
-                    },
-                    path=snapdir / f"{epoch}-{i}.png",
-                    size=5,
-                    vrange=(0, 1),
-                )
-        cprint("Alphas=", model.alphas.cpu().detach().numpy())
+        # loss = -psnr(f2c, gt.to(device), data_range=1.0)
+        # loss = -criterion(f2c.unsqueeze(1), gt.to(device).unsqueeze(1))
+        loss_ssim = -criterion(f2c.unsqueeze(1), gt.to(device).unsqueeze(1))
+        loss_mse = mse(f2c, gt.to(device))
+        loss_reg = lambda_reg * torch.mean((model.alphas - init_alphas) ** 2)
+        loss = loss_ssim + lambda_mse * loss_mse + loss_reg
+
+        loss.backward()
+        optimizer.step()
+        with torch.no_grad():
+            model.alphas.clamp_(0.0, 1.0)
+
+        pbar.set_description(f"[{i}/{len(dataloader)}] Loss={loss.item():.5f}")
