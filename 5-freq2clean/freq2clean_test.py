b93b15c1d36ff60d0a5f8d5ab13a69e416de1a4a
MrPio <valeriomorelli50@gmail.com>
2025-11-19 14:57:23 +0100

    Add metrics plot on 15-15 150-150 15-150 150-15. They look good!

--- 5-freq2clean/freq2clean_test.py
+++ 5-freq2clean/freq2clean_test.py
@@ -16,8 +16,9 @@ from src import *
 
 # %% Args
 BATCH_SIZE = 1
-SELECTED_TRAINING = "20251118-1205-synthetic_deepcad_15"
-GRID_SEARCH_VERSION = True
+SELECTED_TRAINING = "20251118-1221-synthetic_deepcad_150"
+GRID_SEARCH_VERSION = False
+REPLACE_FREQ0 = True
 DENOISER_VARIANT: str | None = None
 
 # %% Dataset Loading
@@ -30,13 +31,11 @@ y_ = Recording(
     f"dataset/{cfg['dataset_name']}/{cfg['denoiser_name']}{DENOISER_VARIANT or cfg['denoiser_variant']}.tif",
     max_frames=None,
 )
-gt_ = Recording(f"dataset/{cfg['dataset_name']}/gt.tif", max_frames=None)
 
 # %% Normalization
 clog("Normalizing data...")
 x = x.normalized
 y_ = y_.normalized
-gt_ = gt_.normalized
 
 clog("Computing averaged vid/frame...")
 x_bar = uniform_filter1d(x, size=cfg["avg_win"], axis=0, mode="reflect")
@@ -49,8 +48,7 @@ idx = (np.arange(n)[:, None] * cfg["patch_t"] + np.arange(cfg["patch_t"])).astyp
 x = torch.from_numpy(x[idx]).float()
 x_bar = torch.from_numpy(x_bar[idx]).float()
 y = torch.from_numpy(y_[idx]).float()
-gt = torch.from_numpy(gt_[idx]).float()
-testset = TensorDataset(x, x_bar, y, gt)
+testset = TensorDataset(x, x_bar, y)
 dataloader = DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False)
 del x, x_bar  # , y_, gt_
 cprint(f"Testset has", len(idx), "samples.")
@@ -70,11 +68,13 @@ model.eval()
 clog("Running test...")
 f2cs = []
 with torch.no_grad():
+    if REPLACE_FREQ0:
+        model.alphas[0] = 0.85
     if GRID_SEARCH_VERSION:
         model.alphas[0] = 0.85
         model.alphas[1:] = 0
 
-    for x, x_bar, y, gt in tqdm(dataloader):
+    for x, x_bar, y in tqdm(dataloader):
         out = model(y.to(device), x_bar.to(device))
         f2cs.append(out.cpu())
 
@@ -84,4 +84,11 @@ f2c = f2cs.reshape(-1, f2cs.shape[2], f2cs.shape[3]).numpy()
 # %% Saving
 clog("Saving TIFF...")
 tiff_dir = mkdir(f"results/{cfg['dataset_name']}/{cfg['denoiser_name']}{DENOISER_VARIANT or cfg['denoiser_variant']}")
-tiff.imwrite(tiff_dir / f"{SELECTED_TRAINING}{'_grid' if GRID_SEARCH_VERSION else ''}.tiff", f2c)
+tiff.imwrite(
+    (
+        tiff_dir / "grid.tiff"
+        if GRID_SEARCH_VERSION
+        else tiff_dir / f"{SELECTED_TRAINING}{'_0.85' if not GRID_SEARCH_VERSION and REPLACE_FREQ0 else ''}.tiff"
+    ),
+    f2c,
+)
